8de83eb447c66e191fc2520f48247e94c9a196c3
Trung Hieu Nguyen <nguyentrunghieu1495@gmail.com>
2019-09-04 23:26:02 +0200

    My first commit

---
+++ app/src/main/java/com/example/myar/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.myar;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.SharedMemory;
+import android.view.Display;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.MaterialFactory;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.ShapeFactory;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArFragment arFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
+
+        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+            Anchor anchor = hitResult.createAnchor();
+
+
+           /* MaterialFactory.makeOpaqueWithColor(this, new Color(android.graphics.Color.RED))
+                    .thenAccept(material -> {
+                        ModelRenderable renderable = ShapeFactory.makeSphere(1.0f, new Vector3(0f, 1f, 1f), material);
+
+                        AnchorNode anchorNode = new AnchorNode(anchor);
+                        anchorNode.setRenderable(renderable);
+                        arFragment.getArSceneView().getScene().addChild(anchorNode);
+                    });
+        });
+    }
+}*/
+
+              ModelRenderable.builder()
+                    .setSource(this, Uri.parse("Knife_01.sfb"))
+                    .build()
+                    .thenAccept(modelRenderable -> addModelToScene(anchor, modelRenderable))
+                    .exceptionally(throwable -> {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                        builder.setMessage(throwable.getMessage())
+                                .show();
+                        return null;
+                    });
+        });
+    }
+
+    private void addModelToScene(Anchor anchor, ModelRenderable modelRenderable) {
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        TransformableNode transformableNode = new TransformableNode(arFragment.getTransformationSystem());
+        transformableNode.setParent(anchorNode);
+        transformableNode.setRenderable(modelRenderable);
+        arFragment.getArSceneView().getScene().addChild(anchorNode);
+        transformableNode.select();
+    }
+}
