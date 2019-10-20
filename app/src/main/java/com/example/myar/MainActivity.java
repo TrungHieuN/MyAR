1b6b82da1d3432d288927029ab82d872b35d75ec
Trung Hieu Nguyen <nguyentrunghieu1495@gmail.com>
2019-10-20 19:31:22 +0200

    Merge branch 'master' of https://github.com/TrungHieuN/MyAR
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/com/example/myar/MainActivity.java

--- app/src/main/java/com/example/myar/MainActivity.java
+++ app/src/main/java/com/example/myar/MainActivity.java
@@ -11,12 +11,10 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.ar.core.Anchor;
-import com.google.ar.core.Frame;
+
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
 
-import com.google.ar.core.Pose;
-import com.google.ar.core.Trackable;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.Camera;
 import com.google.ar.sceneform.Node;
@@ -24,7 +22,7 @@ import com.google.ar.sceneform.Sun;
 import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ModelRenderable;
-import com.google.ar.sceneform.rendering.PlaneRenderer;
+
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
@@ -36,20 +34,18 @@ public class MainActivity extends AppCompatActivity {
 
     private ArFragment arFragment;
     public Plane.Type planeType;
-    private Anchor anchor;
+    private ModelRenderable modelRenderable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.clearButton);
-        button.setOnClickListener(view -> onClear());
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
 
         Objects.requireNonNull(arFragment).setOnTapArPlaneListener((HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
 
-         //Renderable mode in AR app
+            //Renderable mode in AR app
 
             ModelRenderable.builder()
                     .setSource(this, Uri.parse("ArcticFox_Posed.sfb"))
@@ -62,10 +58,15 @@ public class MainActivity extends AppCompatActivity {
                         return null;
                     });
         });
+        Button clearButton = findViewById(R.id.clearButton);
+        clearButton.setOnClickListener(view -> onClear());
+
+        Button deleteButton = findViewById(R.id.deleteButton);
+        deleteButton.setOnClickListener(view -> removeAnchorNode());
     }
 
     private void addModelToScene(ModelRenderable modelRenderable, HitResult hitResult, Plane.Type planeType) {
-        anchor = hitResult.createAnchor();
+        Anchor anchor = hitResult.createAnchor();
         AnchorNode anchorNode = new AnchorNode(anchor);
         anchorNode.setParent(arFragment.getArSceneView().getScene());
         TransformableNode transformableNode = new TransformableNode(arFragment.getTransformationSystem());
@@ -87,42 +88,7 @@ public class MainActivity extends AppCompatActivity {
         } else {
             transformableNode.setRenderable(modelRenderable);
         }
-
     }
-
-
-    @SuppressWarnings("unused")
-    private MotionEvent getNewTapOnBottomCloud(MotionEvent tap, Frame frame) {
-        if (tap == null) {
-            return null;
-        }
-        for (int i = 0; i < 70; i++) {
-            for (HitResult hitResult : frame.hitTest(tap)) {
-                Trackable trackable = hitResult.getTrackable();
-                if (trackable instanceof Plane && ((Plane) trackable).isPoseInPolygon(hitResult.getHitPose()) && ((Plane) trackable).getType() == Plane.Type.HORIZONTAL_UPWARD_FACING) {
-
-                    float distance = PlaneRenderer.class(hitResult.getHitPose(), Objects.requireNonNull(arFragment.getArSceneView().getArFrame()).getCamera().getPose());
-                    anchor = hitResult.createAnchor();
-                    ModelRenderable.builder()
-                            .setSource(this, Uri.parse("ArcticFox_Posed.sfb"))
-                            .build()
-                            .thenAccept(modelRenderable ->addModelToScene(modelRenderable, hitResult, planeType))
-                            .exceptionally(throwable -> {
-                                Toast toast = Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG);
-                                toast.setGravity(Gravity.CENTER, 0, 0);
-                                toast.show();
-                                return null;
-                            });
-                }
-            }
-            tap.offsetLocation(-1 * 15, 0);
-            if (tap.getX() < -100) {
-                break;
-            }
-        }
-        return tap;
-    }
-
 
     private void onClear() {
         List<Node> children = new ArrayList<>(arFragment.getArSceneView().getScene().getChildren());
@@ -137,4 +103,33 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+     private void removeAnchorNode(AnchorNode anchorNode, Anchor newAnchor) {
+         AnchorNode newAnchorNode = null;
+
+         if (anchorNode != null && newAnchor != null) {
+             // Create a new anchor node and move the children over.
+             newAnchorNode = new AnchorNode(newAnchor);
+             newAnchorNode.setParent(arFragment.getArSceneView().getScene());
+             List<Node> children = new ArrayList<>(anchorNode.getChildren());
+             for (Node child : children) {
+                 child.setParent(newAnchorNode);
+             }
+         } else if (anchorNode == null && newAnchor != null) {
+             // First anchor node created, add Andy as a child.
+             newAnchorNode = new AnchorNode(newAnchor);
+             newAnchorNode.setParent(arFragment.getArSceneView().getScene());
+
+             Node andy = new Node();
+             andy.setRenderable(modelRenderable);
+             andy.setParent(newAnchorNode);
+         } else {
+             // Just clean up the anchor node.
+             if (anchorNode != null && anchorNode.getAnchor() != null) {
+                 anchorNode.getAnchor().detach();
+                 anchorNode.setParent(null);
+             }
+         }
+     }
+
 }
