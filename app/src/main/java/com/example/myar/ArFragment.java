e03f09b294d607b14dcbc6103237c1c31b3f7d29
Trung Hieu Nguyen <nguyentrunghieu1495@gmail.com>
2019-11-30 01:41:13 +0100

    Merge branch 'master' of https://github.com/TrungHieuN/MyAR
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/com/example/myar/MainActivity.java

---
+++ app/src/main/java/com/example/myar/ArFragment.java
@@ -0,0 +1,17 @@
+package com.example.myar;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ArFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ar_preview_layout);
+
+
+    }
+}
